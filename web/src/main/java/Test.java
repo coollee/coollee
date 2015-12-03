5eb2ef127ded5c5a23136ec83144a1d3f1dd8c12
coollee <ssangyop@gmail.com>
2015-12-03 21:40:05 +0900

    fixed

--- web/src/main/java/Test.java
+++
@@ -1,5 +0,0 @@
-/**
- * Created by coollee on 2015. 11. 28..
- */
-public class Test {
-}
---
+++ web/src/test/java/WebTest.java
@@ -0,0 +1,8 @@
+import static org.junit.Assert.*;
+
+/**
+ * Created by coollee on 2015. 12. 3..
+ */
+public class WebTest {
+
+}
